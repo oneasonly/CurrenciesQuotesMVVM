8775ea424b702a8ec5d0b6bbdb35e42168bf9dba
HomePC <HomePC@DESKTOP-J269GUU>
2020-06-18 11:27:50 +0300

    adding SearchCurrency module, stage1;

--- ViewVMWPF/ViewModels/SearchCurrencyViewModel.cs
+++ ViewVMWPF/ViewModels/SearchCurrencyViewModel.cs
@@ -1,4 +1,6 @@
-﻿using Prism.Mvvm;
+﻿using Model.SerializationJSON;
+using Prism.Commands;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,5 +11,34 @@ namespace ViewVMWPF.ViewModels
 {
     public class SearchCurrencyViewModel : BindableBase
     {
+        #region fields
+        private string _inputSearch = string.Empty;
+        private List<CbrValute> _preferedCurrencies = new List<CbrValute>();
+        #endregion
+        public SearchCurrencyViewModel()
+        {
+            SearchCommand = new DelegateCommand(OnSearchCommand);
+        }
+        public string InputSearch
+        {
+            get => _inputSearch;
+            set => SetProperty(ref _inputSearch, value);
+        }
+        public List<CbrValute> PreferedCurrenies
+        {
+            get => _preferedCurrencies;
+            set => SetProperty(ref _preferedCurrencies, value);
+        }
+        DelegateCommand SearchCommand;
+        private void OnSearchCommand()
+        {
+            int code;
+            bool isNumber = int.TryParse(InputSearch, out code);
+            if(isNumber)
+            {
+
+            }
+
+        }
     }
 }
